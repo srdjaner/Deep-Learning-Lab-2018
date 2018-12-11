93554fb82f144ac2db34303ef7c890d26427feab
worstseed <maciejanowski@gmail.com>
2018-12-11 17:19:53 +0100

    CHANGE dqn_agent.py with justification: act_values[0], act fun. CHANGE argmax to amax in same script, but train fun

--- exercise4/dqn/dqn_agent.py
+++ exercise4/dqn/dqn_agent.py
@@ -22,6 +22,7 @@ class DQNAgent:
         self.Q_target = Q_target
 
         self.epsilon = epsilon
+        self.epsilon_decay = 0.995
         self.epsilon_min = 0.05
 
         self.num_actions = num_actions
@@ -49,7 +50,7 @@ class DQNAgent:
         # 2. sample next batch and perform batch update:
         batch_states, batch_actions, batch_next_states, batch_rewards, batch_dones = self.replay_buffer.next_batch(self.batch_size)
         #       2.1 compute td targets:
-        td_targets =  batch_rewards + self.discount_factor * np.argmax(self.Q_target.predict(self.sess, batch_next_states), axis=1)
+        td_targets =  batch_rewards + self.discount_factor * np.amax(self.Q_target.predict(self.sess, batch_next_states), axis=1)
         #       2.2 update the Q network
         loss = self.Q.update(self.sess, batch_states, batch_actions, td_targets)
         # print("loss Q net: ", loss)
@@ -57,7 +58,7 @@ class DQNAgent:
         self.Q_target.update(self.sess)
         #print("loss:", loss)
         if self.epsilon > self.epsilon_min:
-           self.epsilon *= 0.999
+           self.epsilon *= self.epsilon_decay
         #print("epsilon: ", self.epsilon)
 
 
@@ -75,7 +76,11 @@ class DQNAgent:
             # TODO: take greedy action (argmax)
             #state = np.reshape(state, (1,4))
             act_values = self.Q.predict(self.sess, [state]) #it was q target
-            action_id = np.argmax(act_values)
+            # we should be using act_values[0], i guess
+            # print("act values: ", act_values)             # act values:  [[0.05641035 0.06138265]]
+            # print("act values[0]: ", act_values[0])       # act values[0]:  [0.05641035 0.06138265]
+
+            action_id = np.argmax(act_values[0])
 
             # print("I predicted an action.")
         else:
@@ -85,7 +90,7 @@ class DQNAgent:
             # Hint for the exploration in CarRacing: sampling the action from a uniform distribution will probably not work.
             # You can sample the agents actions with different probabilities (need to sum up to 1) so that the agent will prefer to accelerate or going straight.
             # To see how the agent explores, turn the rendering in the training on and look what the agent is doing.
-        print("action_id: ", action_id)
+        # print("action_id: ", action_id)
         return action_id
 
 
